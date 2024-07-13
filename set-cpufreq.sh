ea7f31be0a4c1b897929ef817be9d7bcf4edf517
John Butcher <1butcherjohn@gmail.com>
2024-07-13 14:40:29 -0400

    add sudo to command

--- set-cpufreq.sh
+++ set-cpufreq.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# This script must be run prefixed with 'sudo' for admin rights
-
 for cpu in /sys/devices/system/cpu/cpu[0-9]*; do  
-  cpufreq-set -c "${cpu##*/cpu}" -g performance  
+  sudo cpufreq-set -c "${cpu##*/cpu}" -g performance  
 done   
